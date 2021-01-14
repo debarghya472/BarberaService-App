3f40c908c30abdb5947bcba4163a5a7e945796ba
debarghya472 <debarghya_1901me19@iitp.ac.in>
2021-01-14 21:28:44 +0530

    save work

--- app/src/main/java/com/barbera/barberaserviceapp/ui/profile/ProfileFragment.java
+++ app/src/main/java/com/barbera/barberaserviceapp/ui/profile/ProfileFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,9 +14,30 @@ import com.barbera.barberaserviceapp.R;
 
 public class ProfileFragment extends Fragment {
 
+    private TextView name;
+    private TextView email;
+    private TextView phone;
+    private TextView earnings;
+    private TextView points;
+    private  TextView trips;
+
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_profile,container,false);
+       View view= inflater.inflate(R.layout.fragment_profile,container,false);
+       name = (TextView)view.findViewById(R.id.NameInProfile);
+       email = (TextView)view.findViewById(R.id.EmailInProfile);
+       phone =(TextView)view.findViewById(R.id.PhoneInProfile);
+       earnings =(TextView)view.findViewById(R.id.earning);
+       points =(TextView)view.findViewById(R.id.points);
+       trips =(TextView)view.findViewById(R.id.trip);
+       
+       displayProfile();
+       return view;
     }
+
+    private void displayProfile() {
+    }
+
 }
