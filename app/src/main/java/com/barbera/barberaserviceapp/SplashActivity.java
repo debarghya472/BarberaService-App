4d0c103b104c6b12bf2e1e1676bbf2abc2ae5e22
debarghya472 <debarghya_1901me19@iitp.ac.in>
2021-01-26 15:09:59 +0530

    added animation

--- app/src/main/java/com/barbera/barberaserviceapp/SplashActivity.java
+++ app/src/main/java/com/barbera/barberaserviceapp/SplashActivity.java
@@ -5,15 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.barbera.barberaserviceapp.ui.login.LoginActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
+    ImageView logo;
+    Animation grow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        logo = (ImageView) findViewById(R.id.logo);
+        grow = AnimationUtils.loadAnimation(this,R.anim.grow);
+        logo.setAnimation(grow);
     }
     @Override
     protected void onStart() {
