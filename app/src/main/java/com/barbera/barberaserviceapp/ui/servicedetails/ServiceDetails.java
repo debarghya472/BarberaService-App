7a0b717a313b69351ec632a00aee105c4957823c
debarghya472 <debarghya_1901me19@iitp.ac.in>
2021-03-29 02:04:37 +0530

    checking

---
+++ app/src/main/java/com/barbera/barberaserviceapp/ui/servicedetails/ServiceDetails.java
@@ -0,0 +1,16 @@
+package com.barbera.barberaserviceapp.ui.servicedetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.barbera.barberaserviceapp.R;
+
+public class ServiceDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_service_details);
+    }
+}
