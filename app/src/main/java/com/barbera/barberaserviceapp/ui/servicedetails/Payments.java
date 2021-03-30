e1ac641dca9b2d4186b595494dfff0689b1b35c4
debarghya472 <debarghya_1901me19@iitp.ac.in>
2021-03-30 14:18:31 +0530

    for kush

---
+++ app/src/main/java/com/barbera/barberaserviceapp/ui/servicedetails/Payments.java
@@ -0,0 +1,16 @@
+package com.barbera.barberaserviceapp.ui.servicedetails;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.barbera.barberaserviceapp.R;
+
+public class Payments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_payments);
+    }
+}
